27972fe0cfae581e005f02351bbf8ff3056e2b55
Akshay Nair <phenax5@gmail.com>
2023-12-01 14:50:33 +0530

    chore: init commit got charlie bit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      shell = { pkgs, system }: with pkgs; mkShell (rec {
+        buildInputs = with pkgs; [
+          cbqn
+          just
+        ];
+        nativeBuildInputs = [ clang ];
+
+        LD_LIBRARY_PATH = "${lib.makeLibraryPath (buildInputs ++ nativeBuildInputs)}";
+      });
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in
+      {
+        devShells.default = shell { inherit pkgs system; };
+      });
+}
---
+++ justfile
@@ -0,0 +1,3 @@
+
+watch day:
+  npx nodemon --exec "clear && bqn -f src/{{day}}/main.bqn" -e .bqn
